feb78a816f946b13c0b33d92ced1896207580a8b
Anton <tonhyjohns@gmail.com>
2023-03-29 20:21:29 +0300

    GULP 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
-// Static Server + watching scss/html files
-gulp.task('serve', gulp.series('sass', function () {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("./src/scss/*.scss", ['sass']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
-}));
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-    return gulp.src("./src/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./build"))
+// Sass compilation task
+gulp.task('sass', () => {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./build'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('start', gulp.series('sass', 'serve'));
+// Watch task
+gulp.task('watch', () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./src/scss/*.scss', gulp.series('sass'));
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+// Default task
+gulp.task('start', gulp.series('sass', 'watch'));
